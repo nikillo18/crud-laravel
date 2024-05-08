b4379a93e55a7a3b7200c1ddd7602492ae888de9
nikillo18 <nicodrax18@gmail.com>
2024-05-08 19:49:46 -0300

    subido

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
-use App\Http\Requests\students;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentsController;
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,12 +14,18 @@ use App\Http\Controllers\StudentsController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::resource('students', StudentsController::class);
 
-
- 
-
-
+require __DIR__.'/auth.php';
