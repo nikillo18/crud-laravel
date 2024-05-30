1b1668337bbeba337f587e64387b3d4b213c8b27
nikillo18 <nicodrax18@gmail.com>
2024-05-30 15:38:20 -0300

    arreglo de algo

--- database/migrations/2024_04_24_211928_create_students_table.php
+++ database/migrations/2024_04_24_211928_create_students_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('apellido', 50);
             $table->string('nombre', 50);
             $table->date('nacimiento');
-            $table->integer('asisst');
+            $table->integer('asisst')->default(0);
             $table->timestamps();
         });
     }
